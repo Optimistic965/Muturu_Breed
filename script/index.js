bad36b5407c124baf600e9fda9406d9e16f8b973
Ifeoluwa <onabajoif@yahoo.com>
2023-03-28 04:38:16 +0100

    Began work on the functionanlity

--- script/index.js
+++ script/index.js
@@ -1,4 +1,19 @@
+const img = ['mainHeroBg.svg', 'mainHeroBg2.svg', 'mainHeroBg3.svg']
+const homeHero = document.getElementsByClassName('main__cont')
+
+function changeBg(){
+    for(let i = 0; i < img.length; i++){
+        if(homeHero){
+            homeHero.style.background = `url(../media/${img[i]})`
+        }
+    }
+}
+
 $(document).ready(function(){
+    document.body.style.zoom = "90%"
     console.log('document is ready')
     $('#team_page #tab_cont').tabs()
+    // setInterval(changeBg(), 3000)
+    console.log(homeHero)
+    $('#home .hero .main__cont div')
 })
