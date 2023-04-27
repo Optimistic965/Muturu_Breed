25249ec2207d0023eed984650d5f99aa4ce2da20
Ifeoluwa <onabajoif@yahoo.com>
2023-04-27 14:09:48 +0100

    navbar and modal popup

--- script/index.js
+++ script/index.js
@@ -2,38 +2,14 @@ $(document).ready(function(){
     $('#team_page #tab_cont').tabs()
 })
 
-// Getting element with field by ID
-const uName = document.getElementById('name')
-const mail = document.getElementById('mail')
-const phNum = document.getElementById('phNum')
-const msg = document.getElementById('msg')
-// Submit button selector
-const submit = document.getElementById('submit')
+const hamburger = document.querySelector('.burger_cont')
+const navMenu = document.querySelector('.nav__item__cont')
 
-// Validate FormFields
-// Just making sure it's not empty
-function validate(){
-    if(uName.value.length < 1 || mail.value.length < 1 || phNum.value.length < 1 || msg.value.length < 1){
-        return false
-    } else {
-        return true
-    }
-}
-
-function handleSumbit(e) {
-    e.preventDefault()
-    const valid = validate()
-    // Creating an object to be submitted 
-    const data = {}
-    if(valid){
-        data['Name'] = uName.value
-        data['Mail'] = mail.value
-        data['Phone Number'] = phNum.value
-        data['Message'] = msg.value
-        console.log(data)
-    } else {
-        alert('field can\'t be empty')
-    }
-}
-// Submit Event listener
-submit.addEventListener('click', handleSumbit)
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+document.querySelectorAll('.nav__item').forEach(element => element.addEventListener('click', () => {
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+}))
